05ce06936433e61e0a9ec412d0ee9beae02ea0cb
Somin <sominupadhyaya@gmail.com>
2022-09-04 20:41:25 +0545

    end queries for creating table and inserting values

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,28 +1,29 @@
 -- Creating the table
 
--- create table students(
---     id serial primary key,
---     name varchar(255) not null,
---     roll_no int not null,
---     email varchar(255)
--- )
+create table students(
+    id serial primary key,
+    name varchar(255) not null,
+    roll_no int not null,
+    email varchar(255)
+)
 
 -- Inserting data in the rows
 
--- insert into students
--- (
---     name,
---     roll_no,
---     email
--- )
--- values(
---     'romin',
---     1,
---     'romin123@gmail.com'
--- ),
--- (
---     'john',
---     2,
---     'john234@gmail.com'
--- )
+insert into students
+(
+    name,
+    roll_no,
+    email
+)
+values(
+    'romin',
+    1,
+    'romin123@gmail.com'
+),
+(
+    'john',
+    2,
+    'john234@gmail.com'
+)
+-- Selecting the values from table
 select * from students;
