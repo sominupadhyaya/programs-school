6ff83038ccd3d660f542a904e0fc859dbf884c15
Somin <sominupadhyaya@gmail.com>
2022-09-05 20:20:55 +0545

    add updating a record

---
+++ sql/update.sql
@@ -0,0 +1,10 @@
+--updating a record
+
+
+update students set name = 'bomin' 
+where id = 1;
+
+update students set email = 'bomin123@gmail.com'
+where id = 1; 
+
+--updating mutliple records at a time
