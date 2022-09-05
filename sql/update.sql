0f99dfe45904ed61c78526e0b0ad57bc040fc11b
Somin <sominupadhyaya@gmail.com>
2022-09-05 20:24:17 +0545

    complete basics of update command

--- sql/update.sql
+++ sql/update.sql
@@ -7,3 +7,7 @@ update students set email = 'bomin123@gmail.com'
 where id = 1; 
 
 --updating mutliple records at a time
+
+update students set name = 'johndoe' ,
+email = 'johndoe234@gmail.com'
+where id = 2;
