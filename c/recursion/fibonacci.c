dee494e4ee433313b59046d1af5e52018b8e31b5
Salman Ali <sa1713791@gmail.com>
2022-12-26 23:08:20 +0545

    add recursion

---
+++ c/recursion/fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int fibo(int n) {
+    if (n == 0 || n == 1) {
+        return n;
+    }
+    return fibo(n-1) + fibo(n-2);
+}
+
+int main() {
+    int a;
+    printf("Enter the fibonacci limit:\n");
+    scanf("%d", &a);
+    for(int i=0; i<a; i++) {
+        printf("%d\t", fibo(i));
+    }
+    return 0;
+}
