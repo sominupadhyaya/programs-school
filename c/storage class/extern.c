b2e4de562ac616b20da2ba6cea5663f9e2e182b8
Salman Ali <sa1713791@gmail.com>
2022-12-26 22:14:59 +0545

    added storage class

---
+++ c/storage class/extern.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+// variable that is to be made extern, default value is 0
+int x;
+
+
+void change_value() {
+    // telling the compiler that `x` has already been defined somewhere else
+    extern int x;
+    // value of x is modified
+    x = 50;
+}
+
+int main() {
+    printf("Declared value of x: %d\n", x);
+    change_value();
+    printf("New value of x: %d\n", x);
+    return 0;
+}
