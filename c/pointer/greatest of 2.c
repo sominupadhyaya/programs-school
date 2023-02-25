081395c3771d7a0f93114aef3aac7b9fdf1dd9a7
Salman Ali <sa1713791@gmail.com>
2023-02-25 16:56:28 +0545

    add greatest of 2

---
+++ c/pointer/greatest of 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "../print_ownership.h"
+
+
+int main() {
+    int x, y, *p, *q;
+    printf("Enter two numbers\n");
+    scanf("%d%d", &x, &y);
+    p = &x;
+    q = &y;
+    if (*p > *q) {
+        printf("%d is greatest\n", *p);
+    } else {
+        printf("%d is greatest\n", *q);
+    }
+    print_ownership();
+    return 0;
+}
